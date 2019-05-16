da7aabecc2abe664590950557733eb22c2426708
Satyajith J <satyajithj@protonmail.com>
2019-05-16 18:46:29 +0200

    COCO to PASCAL VOC with bbox

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dataset-convert
-Tools to convert standard dataset formats to another
+# Dataset Convert
+Tools to convert dataset formats from one standard to another.
+
+### Usage
+
+```
+python3 data_convert.py --data_dir=<PATH-TO-INPUT-DATASET> --in_format=<INPUT-DATASET-FORMAT> --out_format=<OUTPUT-DATASET-FORMAT> --out_dir=<PATH-TO-OUTPUT-DATASET> 
+```
+
+### Current support
+
+[x] COCO -> PASCAL VOC (bbox only)
--- convert_utils/from_coco.py
+++ convert_utils/from_coco.py
@@ -5,7 +5,17 @@ import glob
 import datetime
 
 import json
+from lxml import etree
 from tqdm import tqdm
+
+
+def get_coco_category_name(categories, category_id):
+    for cat in categories:
+        if cat['id'] == category_id:
+            return cat['name']
+
+    print('Category unknown for ID', category_id)
+    return 'Unknown'
 
 
 def coco2pascalvoc(in_dir, out_dir):
@@ -20,7 +30,7 @@ def coco2pascalvoc(in_dir, out_dir):
         print("Warning: The output directory '{}' does not exist".format(out_dir))
         print("Using the default directory '{}'".format(in_dir))
         out_dir = in_dir
-    
+
     # Add a path separator at the end for consistency.
     if in_dir[-1] != os.sep:
         in_dir = in_dir + os.sep
@@ -50,4 +60,78 @@ def coco2pascalvoc(in_dir, out_dir):
 
     # Process the COCO annotation files and create the PASCAL VOC annotations.
     for i in tqdm(range(num_ann_files)):
-        pass
+        # Load the i-th annotation file.
+        ann_file = open(ann_file_names[i], "r")
+        coco_ann_data = json.load(ann_file)
+
+        # Get category data.
+        ann_categories = coco_ann_data['categories']
+
+        for img in coco_ann_data['images']:
+            # Iterate over images in the i-th annotation file.
+            image_id = img['id']
+            category_ids = img['category_ids']
+
+            pvoc_ann = {}
+            pvoc_ann['root'] = etree.Element("annotation")
+
+            # Add file name subelement
+            pvoc_ann['filename'] = etree.SubElement(pvoc_ann['root'], "filename")
+            pvoc_ann['filename'].text = img['file_name']
+
+            # Image size subelement.
+            pvoc_ann['size'] = etree.SubElement(pvoc_ann['root'], "size")
+            pvoc_ann['width'] = etree.SubElement(pvoc_ann['size'], "width")
+            pvoc_ann['height'] = etree.SubElement(pvoc_ann['size'], "height")
+            pvoc_ann['depth'] = etree.SubElement(pvoc_ann['size'], "depth")
+
+            pvoc_ann['width'].text = str(img['width'])
+            pvoc_ann['height'].text = str(img['height'])
+            # Image depth is set to 3 for now.
+            pvoc_ann['depth'].text = "3"
+
+            # Is segmented? True for COCO.
+            # TODO: Add segmentations.
+            pvoc_ann['segmented'] = etree.SubElement(pvoc_ann['root'], "segmented")
+            pvoc_ann['segmented'].text = "0"    # ! For COCO the annotations are segmented
+
+            # ! PASCAL VOC contains segmented annotations as well.
+            # ! We currently focus on bounding boxes.
+
+            # Add object annotations
+            for ann in coco_ann_data['annotations']:
+                # If the object is not for the current image.
+                if ann['image_id'] != image_id:
+                    pass
+
+                obj = {}
+                obj['root'] = etree.SubElement(pvoc_ann['root'], "object")
+                # Add object name
+                obj['name'] = etree.SubElement(obj['root'], "name")
+                obj['name'].text = get_coco_category_name(ann_categories, ann['category_id'])
+
+                # Truncated and Difficult elements
+                obj['truncated'] = etree.SubElement(obj['root'], "truncated")
+                obj['truncated'].text = "0"
+                obj['difficult'] = etree.SubElement(obj['root'], "difficult")
+                obj['difficult'].text = "0"
+
+                # Add bounding box coordinates
+                obj['bndbox'] = etree.SubElement(obj['root'], "bndbox")
+                obj['xmin'] = etree.SubElement(obj['bndbox'], "xmin")
+                obj['ymin'] = etree.SubElement(obj['bndbox'], "ymin")
+                obj['xmax'] = etree.SubElement(obj['bndbox'], "xmax")
+                obj['ymax'] = etree.SubElement(obj['bndbox'], "ymax")
+
+                obj['xmin'].text = str(ann['bbox'][0])
+                obj['ymin'].text = str(ann['bbox'][1])
+                obj['xmax'].text = str(ann['bbox'][2])
+                obj['ymax'].text = str(ann['bbox'][3])
+
+            # Write the annotation data to its XML file.
+            pvoc_ann_file_name = img['file_name'].split(".")[0] + ".xml"
+            pvoc_ann_file = open(out_ann_dir + pvoc_ann_file_name, "wb")
+            pvoc_ann_file.write(etree.tostring(pvoc_ann['root'], pretty_print=True))
+            pvoc_ann_file.close()
+    
+    # End of coco2pascalvoc()
